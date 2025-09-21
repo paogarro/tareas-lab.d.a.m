a6b4b1ad1be45afef4821e9a4726c07f167d9dbe
paogarro <paolagarro1990@gmail.com>
2025-09-21 15:23:50 -0600

    nueva practica

---
+++ practica5.1 .py
@@ -0,0 +1,6 @@
+#ACTIVIDAD 01:
+#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
+
+palabra = input("Ingrese una palabra: ")
+for i in range(10):
+    print(f"{palabra}")
