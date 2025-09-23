c5844395b2966a9bdba6963d1c868ca372de55b5
paogarro <paolagarro1990@gmail.com>
2025-09-23 12:06:09 -0600

    nueva practica

---
+++ practica8.2 .py
@@ -0,0 +1,10 @@
+#Practica 8
+#ACTIVIDAD 02:
+#Definir una lista con al menos cinco números enteros. Luego, imprimir por
+#pantalla el doble de cada número.
+
+numeros = [5, 10, 15, 20, 25]
+
+for numero in numeros:
+    doble_numero = numero * 2
+print(f"el doble de {numero} es: {doble_numero}")
